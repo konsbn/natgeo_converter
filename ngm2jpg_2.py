b8bf11c95e25062ae387607a3553dddd59116d3a
konsbn <shubphotons@gmail.com>
2021-09-14 20:24:30 +0530

    Multicore Code

---
+++ ngm2jpg_2.py
@@ -0,0 +1,35 @@
+"""
+Converter for NGM files to JPG
+"""
+import os
+from io import BytesIO
+from PIL import Image
+
+
+def convert(src: str, dst: str = None) -> None:
+    """
+    Converts the .NGM file to .JPG by XORing with 239 and writes it to disk
+    """
+    if dst is None:
+        # Given no path for output simply make a new file with
+        # same name and jpg extension
+        dst = os.path.splitext(src)[0] + ".jpg"
+    with open(src, "rb") as f:  # Read the file into bytearray
+        ngm = bytearray(f.read())
+    new_ngm = bytearray([i ^ 239 for i in ngm])  # Byte wise XOR with 239
+    image = Image.open(BytesIO(new_ngm))
+    image.save(dst)
+
+
+if __name__ == "__main__":
+    import sys
+    import glob
+
+    try:
+        cng_list = glob.glob(sys.argv[1] + "*.cng")
+    except IndexError:
+        print("Incomplete List Of Arguments, Please supply the Directory")
+        sys.exit(1)
+
+    for cng in cng_list:
+        convert(cng)
