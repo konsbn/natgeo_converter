ee5596007d26580fcb10463635326ade0af4ea56
konsbn <shubphotons@gmail.com>
2021-09-14 20:23:26 +0530

    Minor Changes to reading from stdout

--- ngm2jpg.py
+++ ngm2jpg.py
@@ -6,25 +6,33 @@ from io import BytesIO
 from PIL import Image
 
 
-def convert(src: str, dst: str = "./") -> None:
+def convert(src: str, dst: str = None) -> None:
     """
     Converts the .NGM file to .JPG by XORing with 239 and writes it to disk
     """
-    if dst == "./":
+    if dst is None:
         # Given no path for output simply make a new file with
         # same name and jpg extension
-        dst = dst + os.path.splitext(os.path.basename(src))[0] + ".jpg"
+        dst = os.path.splitext(src)[0] + ".jpg"
     with open(src, "rb") as f:  # Read the file into bytearray
         ngm = bytearray(f.read())
     new_ngm = bytearray([i ^ 239 for i in ngm])  # Byte wise XOR with 239
-    image = Image.open(BytesIO(new_ngm))  # Write decoded file to JPG
+    image = Image.open(BytesIO(new_ngm))
     image.save(dst)
 
 
 if __name__ == "__main__":
     import sys
+    import glob
 
     try:
-        convert(sys.argv[1], sys.argv[2])
+        cng_list = glob.glob(sys.argv[1] + "*.cng")
     except IndexError:
-        convert(sys.argv[1])
+        print("Incomplete List Of Arguments, Please supply the Directory")
+        sys.exit(1)
+
+    for cng in cng_list:
+        try:
+            convert(cng)
+        except KeyboardInterrupt:
+            sys.exit(1)
