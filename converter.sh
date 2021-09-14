f54f40e322fe9e7bf0020994ddee9a0488b197cc
konsbn <shubphotons@gmail.com>
2021-09-14 23:08:34 +0530

    Increased Image Density Parameter

--- converter.sh
+++ converter.sh
@@ -15,7 +15,7 @@ do
     WIDTH="$(identify "$i"NGM*.jpg | cut -d " " -f3 | cut -d "x" -f 1 | datamash mode 1)" #Find the most common Width
     HEIGHT="$(identify "$i"NGM*.jpg | cut -d " " -f3 | cut -d "x" -f 2 | datamash mode 1)" #Find the most common Height
     convert "$i""NGM*.jpg" -set filename:base "%[basename]" -resize "$WIDTH"x"$HEIGHT"\! "$i""%[filename:base].jpg" #Convert all files to be of the same size
-    convert "$i""NGM*.jpg" -set filename:base "%[basename]"  "$i""%[filename:base].pdf" #Convert into pdf
+    convert -density 600 "$i""NGM*.jpg" -set filename:base "%[basename]"  "$i""%[filename:base].pdf" #Convert into pdf
     fd --type=f --extension=cng --extension=jpg NGM "$i" --exec-batch rm {} #Delete .cng and .jpg to conserve space
 
 done
