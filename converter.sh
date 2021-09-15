ce6d4856f8fbd3ead756c543477ef6f566f448cb
konsbn <shubphotons@gmail.com>
2021-09-15 17:32:19 +0530

    Made the datamash more robust

--- converter.sh
+++ converter.sh
@@ -17,8 +17,8 @@ do
     echo "Converting Files in $i"
     python ngm2jpg.py "$i"
 
-    WIDTH="$(identify "$i"NGM*.jpg | cut -d " " -f3 | cut -d "x" -f 1 | datamash mode 1)" #Find the most common Width
-    HEIGHT="$(identify "$i"NGM*.jpg | cut -d " " -f3 | cut -d "x" -f 2 | datamash mode 1)" #Find the most common Height
+    WIDTH="$(identify "$i"NGM*.jpg | cut -d " " -f3 | cut -d "x" -f 1 | sed '/[a-zA-Z]/d' | datamash mode 1)" #Find the most common Width
+    HEIGHT="$(identify "$i"NGM*.jpg | cut -d " " -f3 | cut -d "x" -f 2 | sed '/[a-zA-Z]/d' | datamash mode 1)" #Find the most common Height
     convert "$i""NGM*.jpg" -set filename:base "%[basename]" -resize "$WIDTH"x"$HEIGHT"\! "$i""%[filename:base].jpg" #Convert all files to be of the same size
     convert -density 600 "$i""NGM*.jpg" -set filename:base "%[basename]"  "$i""%[filename:base].pdf" #Convert into pdf
     fd --type=f --extension=cng --extension=jpg NGM "$i" --exec-batch rm {} #Delete .cng and .jpg to conserve space
